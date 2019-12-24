3bf70e1740a4d524e2ac1e114f367896fd118576
Ravi4Teja <pannururavi@gmail.com>
2019-12-25 00:38:57 +0530

    initial commit

---
+++ pythonprac.py
@@ -0,0 +1,20 @@
+import sys
+from os import rename
+
+import requests
+
+import math
+
+print(sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
+
+
+# print(greet('World'))
+# print(greet('Corey'))
+r = requests.get("https://coreyms.com")
+print(r.status_code)
